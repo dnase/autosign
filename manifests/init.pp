f8748f2e53b11d3edfee73e36d0639e927538bcc
Andrew Nase <drew.nase@drew.nase-C02P61W5G3QT>
2017-08-21 09:05:47 -0400

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+class autosign (
+	String $shared_secret = $::autosign_shared_secret,
+) {
+
+  file { '/etc/puppetlabs/puppet/autosign.rb' :
+    ensure  => file,
+    owner   => 'pe-puppet',
+    group   => 'pe-puppet',
+    mode    => '0700',
+    content => epp('autosign_example/autosign.rb.epp', {shared_secret => $shared_secret}),
+    notify  => Service['pe-puppetserver'],
+  }
+
+  ini_setting { 'autosign script setting':
+    ensure  => present,
+    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    section => 'master',
+    setting => 'autosign',
+    value   => '/etc/puppetlabs/puppet/autosign.rb',
+    notify  => Service['pe-puppetserver'],
+  }
+
+}
